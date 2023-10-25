9f8e87835a84796491e3d35c24271f132519b138
Mark <mark.omuga@student.moringaschool.com>
2023-10-25 18:06:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const sector = document.getElementById("main");
+sector.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Mark is the champion";
+
+document.body.append(newHeader);
